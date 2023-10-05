2ec806d548e2a5513162050f790f9e350c451bfb
IngCherry <andresmrodriguezr@outlook.com>
2023-10-05 13:07:55 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # React.JS
 I´m starting a new path in platzi, now studying frontend with React.JS, in my actual work I use Angular.JS, but i´d like to expand my knowledge in other frameworks.
+
+## Initial Commit
+I´ve copied a basic project configuration from platzi´s github, for now, the objective is to learn the basic components of react, then, I´ll do a project since nothing.
---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './platzi.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Primeras pruebas de la App del curso
+        </p>
+        <TodoItem></TodoItem>
+        <TodoItem></TodoItem>
+        <TodoItem></TodoItem>
+        <TodoItem></TodoItem>
+        <a
+          className="App-link"
+          href="https://platzi.com/reactjs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function TodoItem() {
+  return(
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+export default App;
